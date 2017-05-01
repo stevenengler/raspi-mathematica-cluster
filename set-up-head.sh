de00f4b7b26eaf84a6bcfc24e6069c3d0bdc4354
Steven Engler <smengler3@gmail.com>
2017-04-30 23:12:00 -0400

    Fixed bug from changing hostname.
    Moved the commands to change the hostname in 'set-up-head.sh' to the end of the script, since this caused problems with ssh-keygen.

--- set-up-head.sh
+++ set-up-head.sh
@@ -5,16 +5,16 @@ sudo apt-get upgrade -y
 # Install additional packages
 sudo apt-get install nmap -y
 #
-# Set the hostname for this computer
-sudo sh -c 'echo picluster > /etc/hostname'
-sudo sed -i -e '/127.0.1.1/ s/raspberrypi/picluster/' /etc/hosts
-#
 # Make keys for passwordless ssh (for users pi and student)
 cat /dev/zero | ssh-keygen -q -N ""
 sudo -H -u student bash -c 'cat /dev/zero | ssh-keygen -q -N ""'
 #
 # Show the login screen at startup
 sudo sed -i -e 's/autologin-user=pi/#autologin-user=pi/' /etc/lightdm/lightdm.conf
+#
+# Set the hostname for this computer
+sudo sh -c 'echo picluster > /etc/hostname'
+sudo sed -i -e '/127.0.1.1/ s/raspberrypi/picluster/' /etc/hosts
 #
 # Reboot
 read -p "Press the <enter> key to reboot the computer and complete the setup..."
