28ae365f9225910ccd9b2227a438069f23ed5dd9
Steven Engler <smengler3@gmail.com>
2017-05-02 15:10:39 -0400

    Attempted fix for accepting ssh signature with new hostname.

--- set-up-node.sh
+++ set-up-node.sh
@@ -14,13 +14,18 @@ ssh -tt pi@$NODE_IP './set-up-all.sh'
 ssh pi@$NODE_IP 'rm -f set-up-all.sh'
 ssh pi@$NODE_IP "sudo sh -c \"echo picluster_$NODE_ID > /etc/hostname\""
 ssh pi@$NODE_IP "sudo sed -i -e \"/127.0.1.1/ s/raspberrypi/picluster_$NODE_ID/\" /etc/hosts"
+ssh pi@$NODE_IP "sudo hostname picluster_$NODE_ID"
 #
 # Copy the ssh keys to the node for the student user
 sudo -H -u student bash -c "ssh-copy-id student@$NODE_IP"
 #
+# Add the host name for the new node
+sudo sh -c "echo \"$NODE_IP\tpicluster_$NODE_ID\" >> /etc/hosts"
+#
+# Connect to the node (with both accounts) using the new hostname so that it accepts the new signature
+ssh pi@picluster_$NODE_ID 'echo done (1).'
+sudo -H -u student bash -c "ssh student@picluster_$NODE_ID 'echo done (2).'"
+#
 # Restart the remote node
 echo "The node Pi will now restart. Please be patient when trying to connect to it."
 ssh -n -f pi@$NODE_IP "sh -c 'sudo nohup shutdown -r now > /dev/null 2>&1 &'"
-#
-# Add the host name for the new node
-sudo sh -c "echo \"$NODE_IP\tpicluster_$NODE_ID\" >> /etc/hosts"
